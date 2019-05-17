83d70cff5555d8f5f400bd44533e41e81a084d26
sonkkeli <laurilasonja@gmail.com>
2019-05-17 22:08:43 +0300

    Update TaskGenerator.js

--- src/components/TaskGenerator.js
+++ src/components/TaskGenerator.js
@@ -3,10 +3,15 @@ import './task.css'
 
 const TaskGenerator = ({task, player, handleNextTask}) => {
 
-
+    const whoPlays = () => {
+        if (player){
+            return <p className="display-4">Today's gamer is: {player}</p>
+        }
+    }
 
     return (
         <div className="task-background jumbotron">
+            {whoPlays()}
             <p className="display-2">{task.description}</p>
             <button onClick={handleNextTask} className="btn-dark btn ml-2" >Next plz</button>
         </div>
