d2d6e70728d8da16bfa8276bcaaae84bf47dd8cb
sonkkeli <laurilasonja@gmail.com>
2019-05-17 20:58:57 +0300

    Update TaskGenerator.js

--- src/components/TaskGenerator.js
+++ src/components/TaskGenerator.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-const TaskGenerator = () => {
+const TaskGenerator = ({tasks}) => {
+    const randomTask = () => {
+        const newTask = tasks[Math.floor(Math.random() * tasks.length)]
+        console.log(newTask)
+    }
+    
 
     return (
         <div>
-            
+            <button onClick={randomTask}>Seuraava</button>
         </div>
     )
     
